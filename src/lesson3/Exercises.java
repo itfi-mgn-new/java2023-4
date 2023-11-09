9468136160d83aedab4f0c9f24c9bd590f78bf52
student <student@IMAC-201-101-PC.itf.local>
2023-11-09 18:32:29 +0500

    Daily commit

---
+++ src/lesson3/Exercises.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int[][]	matrix1 = new int[][] {new int[] {1,2,3}, new int[] {4,5,6}, new int[] {7,8,9}};
+		final int[][]	matrix2 = new int[][] {new int[] {10,20,30}, new int[] {11,21,31}, new int[] {12,22,32}};
+
+	}
+
+}
