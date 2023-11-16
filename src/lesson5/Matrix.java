14e6b204cf76892f0c6168012d24b6ed3823d663
chav1961 <chav1961@mail.ru>
2023-11-16 11:12:44 +0500

    Daily commit

---
+++ src/lesson5/Matrix.java
@@ -0,0 +1,43 @@
+package lesson5;
+
+public class Matrix {
+	
+	private int[][]		content;
+	
+	public Matrix() {
+	}
+
+	public static void main(String[] args) {
+		final int[][] m1 = new int[][] {new int[] {1,2,3}, new int[] {4,5,6}, new int[] {7,8,9}};
+		final int[][] m2 = new int[][] {new int[] {10,20,30}, new int[] {40,50,60}, new int[] {70,80,90}};
+		
+		final Matrix	mat1 = new Matrix(), mat2 = new Matrix();
+		
+		mat1.setContent(m1);
+		mat2.setContent(m2);
+		
+		System.err.println("Add="+mat1.add(mat2));
+		System.err.println("Multiply="+mat1.mul(mat2));
+	}
+
+	public Matrix add(final Matrix right) {
+		// TODO:
+	}
+
+	public Matrix mul(final Matrix right) {
+		// TODO:
+	}
+	
+	public int[][] getContent() {
+		// TODO:
+	}
+	
+	public void setContent(final int[][] content) {
+		// TODO:
+	}
+	
+	@Override
+	public String toString() {
+		// TODO:
+	}
+}
