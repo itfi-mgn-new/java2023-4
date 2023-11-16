3b77a7c2bbd352f7f1346dbe4a1ca3672589abfe
Student <Student@IMAC-201-101-PC.itf.local>
2023-11-16 19:35:57 +0500

    Daily commit

---
+++ src/lesson5/Exercise.java
@@ -0,0 +1,39 @@
+package lesson5;
+
+public class Exercise {
+
+	private int currentValue = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println(new Exercise().add(10).mul(20).get());
+	}
+
+	public Exercise add(int value) {
+		
+	}
+	
+	public Exercise sub(int value) {
+		
+	}
+
+	public Exercise mul(int value) {
+		
+	}
+
+	public Exercise div(int value) {
+		
+	}
+	
+	public Exercise mod(int value) {
+		
+	}
+
+	public Exercise set(int value) {
+		
+	}
+	
+	public int get() {
+		
+	}
+}
