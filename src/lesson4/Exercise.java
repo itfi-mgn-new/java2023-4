8580cf59d1cf36e92316b611528eebd39b67c287
Student <Student@IMAC-201-101-PC.itf.local>
2023-11-16 17:54:43 +0500

    Daily commit

---
+++ src/lesson4/Exercise.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.err.println(factorial(5));
+	}
+
+	static int factorial(int x) {
+		// TODO: write factorial code
+	}
+}
