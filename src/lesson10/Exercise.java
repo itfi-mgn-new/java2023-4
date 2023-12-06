f2971ef35d4c4e73e794f92ebf0f1a5150e5b2f4
student <student@IMAC-201-101-PC.itf.local>
2023-12-06 20:18:05 +0500

    Daily commit

---
+++ src/lesson10/Exercise.java
@@ -0,0 +1,12 @@
+package lesson10;
+
+@Value(100)
+public class Exercise {
+	
+	@Value(20)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 
+	}
+
+}
---
+++ src/lesson10/Value.java
@@ -0,0 +1,15 @@
+package lesson10;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ TYPE, FIELD, METHOD })
+public @interface Value {
+	int value();
+}
