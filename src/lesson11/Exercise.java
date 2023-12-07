50968ef8473053e923fbe02e3720a0db29ee92e2
student <student@IMAC-201-101-PC.itf.local>
2023-12-07 20:47:25 +0500

    DAily commit

--- src/lesson11/Exercise.java
+++ src/lesson11/Exercise.java
@@ -7,15 +7,42 @@ public class Exercise {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.err.println("Val="+calculate(...));	// Обычный класс
-		System.err.println("Val="+calculate(...));	// Вложенный класс
-		System.err.println("Val="+calculate(...));  // Анонимный класс
-		System.err.println("Val="+calculate(...));  // Лямбда-выражение
+		Exercise owner = new Exercise();
+		System.err.println("Val="+calculate(new ZZZ()));	// Обычный класс
+		System.err.println("Val="+calculate(owner.new TTT()));	// Вложенный класс
+		System.err.println("Val="+calculate(new CalcInterface() {
+
+			@Override
+			public int calculate(int x, int y) {
+
+				return x * y;
+			}}));  // Анонимный класс
+		System.err.println("Val="+calculate((x,y)->x/y));  // Лямбда-выражение
 	}
 
 	public static int calculate(final CalcInterface calc) {
 		return calc.calculate(100,200);
 	}
+
+	class TTT implements CalcInterface {
+
+		@Override
+		public int calculate(int x, int y) {
+			
+			return x - y;
+		}
+		
+	}
 }
 
+
+class ZZZ implements Exercise.CalcInterface {
+
+	@Override
+	public int calculate(int x, int y) {
+		
+		return x + y;
+	}
+	
+}
 
