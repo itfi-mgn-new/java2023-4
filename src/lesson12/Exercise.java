e1fd264ad563a9828ddae03ef4b86c3b0447809c
Student <Student@IMAC-201-101-PC.itf.local>
2023-12-20 17:31:00 +0500

    Daily commit

--- src/lesson12/Exercise.java
+++ src/lesson12/Exercise.java
@@ -5,6 +5,27 @@ public class Exercise {
 	public static void main(String[] args) {
 		String	s = "abcdefg";
 		// TODO: "AbCdEfG";
+//		StringBuilder r = new StringBuilder();
+//		for(int index = 0; index < s.length(); index++) {
+//			if (index % 2 == 0) { 
+//				r.append(Character.toUpperCase(s.charAt(index)));
+//			}
+//			else {
+//				r.append(Character.toLowerCase(s.charAt(index)));
+//			}
+//		}
+		char[]	r = new char[s.length()];
+		for(int index = 0; index < r.length; index++) {
+			if (index % 2 == 0) { 
+				r[index] = Character.toUpperCase(s.charAt(index));
+			}
+			else {
+				r[index] = Character.toLowerCase(s.charAt(index));
+			}
+			
+		}
+		System.err.println("R="+new String(r));
+		
 	}
 
 }
