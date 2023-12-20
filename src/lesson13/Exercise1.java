b504680db3278675791902d8807ef30c1ac573ef
Student <Student@IMAC-201-101-PC.itf.local>
2023-12-20 18:15:42 +0500

    DAily commit

---
+++ src/lesson13/Exercise1.java
@@ -0,0 +1,14 @@
+package lesson13;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println(toCaps("ivanov ivan ivanovich")); // Ivanov Ivan Ivanovich
+	}
+	
+	static String toCaps(final String source) {
+		// TODO:
+	}
+
+}
