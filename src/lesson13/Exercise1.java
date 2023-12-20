3c3e9cc8fddcddd483e6d83fc4c0a6d151ff7e9f
Student <Student@IMAC-201-101-PC.itf.local>
2023-12-20 19:26:35 +0500

    DAily commit

--- src/lesson13/Exercise1.java
+++ src/lesson13/Exercise1.java
@@ -9,6 +9,45 @@ public class Exercise1 {
 	
 	static String toCaps(final String source) {
 		// TODO:
+//		final StringBuilder sb = new StringBuilder();
+//		
+//		for (String item : source.split(" ")) {
+//			sb.append(' ')
+//				.append(Character.toUpperCase(item.charAt(0)))
+//				.append(item.substring(1).toLowerCase());
+//		}
+//		return sb.substring(1);
+		final char[]	result = new char[source.length()];
+		int				state = 0;
+		
+		for(int index = 0; index < result.length; index++) {
+			final char	symbol = source.charAt(index);
+			
+			switch (state) {
+				case 0 :
+					result[index] = Character.toUpperCase(symbol);
+					state = 1;
+					break;
+				case 1 :
+					if (symbol != ' ') {
+						result[index] = Character.toLowerCase(symbol);
+					}
+					else {
+						state = 2;
+						result[index] = ' ';
+					}
+					break;
+				case 2 :
+					if (symbol == ' ') {
+						result[index] = ' ';
+					}
+					else {
+						state = 0;
+						index--;
+					}
+					break;
+			}
+		}
+		return new String(result);
 	}
-
 }
---
+++ src/lesson13/Exercise2.java
@@ -0,0 +1,13 @@
+package lesson13;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println(toWords(127));	// сто двадцать семь
+	}
+
+	static String toWords(int value) {
+		// TODO:
+	}
+}
