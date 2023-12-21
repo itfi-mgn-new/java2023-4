1446e89032b89862b3cf5e44a50bd0dd5381e98e
student <student@IMAC-201-101-PC.itf.local>
2023-12-21 18:12:32 +0500

    daily commit

---
+++ src/lesson14/Exercise1.java
@@ -0,0 +1,15 @@
+package lesson14;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("3+5+10="+calc("3+5+10"));	// 3+5+10=18
+	}
+	
+	static int calc(final String source) {
+		// TODO: split string by '+' and calculate sum;
+		
+	}
+
+}
