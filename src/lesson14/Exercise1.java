b3ddeb78dc88381b96ccf11858926721ebe1e2d0
student <student@IMAC-201-101-PC.itf.local>
2023-12-21 19:18:25 +0500

    Daily commit

--- src/lesson14/Exercise1.java
+++ src/lesson14/Exercise1.java
@@ -1,5 +1,7 @@
 package lesson14;
 
+import java.util.Arrays;
+
 public class Exercise1 {
 
 	public static void main(String[] args) {
@@ -9,7 +11,23 @@ public class Exercise1 {
 	
 	static int calc(final String source) {
 		// TODO: split string by '+' and calculate sum;
+		// 1:
+//		final String[]	splitted = source.split("\\+");
+	
+//		// 2:
+//		final int[]		temp = new int[splitted.length];
+//		
+//		for(int index = 0; index < temp.length; index++) {
+//			temp[index] = Integer.valueOf(splitted[index]);
+//		}
 		
+		// 3:
+		System.err.println("Content: "+Arrays.toString(source.split("\\+")));
+		int	sum = 0;
+		for(String x : source.split("\\+")) {
+			sum += Integer.valueOf(x);
+		}
+		return sum;
 	}
 
 }
