e477edbabb52114a370d8a61cf5dfcdaf078295a
Student <Student@IMAC-201-101-PC.itf.local>
2024-01-25 20:45:43 +0500

    DAily commit

--- src/lesson20/Exercise.java
+++ src/lesson20/Exercise.java
@@ -1,5 +1,9 @@
 package lesson20;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Exercise {
 	public static final String[]	names = {"ivanov"
 			,"petrov"
@@ -12,7 +16,17 @@ public class Exercise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Map<String,int[]> result = new HashMap<>();
+		
+		for(String item : names) {
+			if (!result.containsKey(item)) {
+				result.put(item,new int[] {0});
+			}
+			result.get(item)[0]++;
+		}
+		for (Entry<String, int[]> item : result.entrySet()) {
+			System.err.println(item.getKey()+"="+item.getValue()[0]);
+		}
 	}
 
 }
