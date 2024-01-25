c12aade8d4aab29ad0dba7bb5e6a72287f55ffa6
Student <Student@IMAC-201-101-PC.itf.local>
2024-01-25 19:53:41 +0500

    Daily commit

---
+++ src/lesson20/Exercise.java
@@ -0,0 +1,18 @@
+package lesson20;
+
+public class Exercise {
+	public static final String[]	names = {"ivanov"
+			,"petrov"
+			,"sidorov"
+			,"ivanov"
+			,"petrov"
+			,"sidorov"
+			,"ivanov"
+			,"petrov"};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
