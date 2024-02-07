d76448c921911661398bec0a6dc2290a076c4709
Student <Student@IMAC-201-101-PC.itf.local>
2024-02-07 20:44:16 +0500

    Daily commit

--- src/lesson21/Exercise.java
+++ src/lesson21/Exercise.java
@@ -1,5 +1,7 @@
 package lesson21;
 
+import java.util.Calendar;
+
 public class Exercise {
 	public static final String FAMILIES = "ivanov ivan ivanovich 24\n" +
 										  "petrov petr petrovich 19\n" +
@@ -8,7 +10,18 @@ public class Exercise {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final Calendar	c = Calendar.getInstance();
+		final int		year = 2024;
+		int	count = 0;
 		
+		c.set(year, Calendar.JANUARY, 1);
+		while(c.get(Calendar.YEAR) == year) {
+			if (c.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+				count++;
+			}
+			c.add(Calendar.DAY_OF_YEAR, 1);
+		}
+		System.err.println("Days: "+count);
 	}
 
 }
