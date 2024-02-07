7aab2af5abc76e5d96f7d7283f4b9e194b07f292
Student <Student@IMAC-201-101-PC.itf.local>
2024-02-07 19:19:03 +0500

    Daily comit

---
+++ src/lesson21/Exercise.java
@@ -0,0 +1,14 @@
+package lesson21;
+
+public class Exercise {
+	public static final String FAMILIES = "ivanov ivan ivanovich 24\n" +
+										  "petrov petr petrovich 19\n" +
+										  "sidorov sidor sidorovich 33";
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
