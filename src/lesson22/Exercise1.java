cd3f557076197be53dda40421788699315a24cda
Student <Student@IMAC-201-101-PC.itf.local>
2024-02-08 18:53:59 +0500

    Daily commmit

---
+++ src/lesson22/Exercise1.java
@@ -0,0 +1,13 @@
+package lesson22;
+
+public class Exercise1 {
+	public static final String	FIOS = "ivanov i i, 12-10-1980\n" +
+									"petrov p p, 03-02-1991\n" +
+									"sidorov s s, 14-11-1978\n";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
